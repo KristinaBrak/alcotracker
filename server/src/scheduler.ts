1518c787f895b9876153212a147d4efb870d5726
mantas.sidabras <sid.mantas@gmail.com>
2021-03-28 20:43:22 +0300

    chaged schedule

--- server/src/scheduler.ts
+++ server/src/scheduler.ts
@@ -3,9 +3,13 @@ import { logger } from './logger';
 
 const schedule = process.env.SCHEDULE ?? '5 7 * * *';
 export const scheduleJob = (fn: () => Promise<any>) => {
-  cron.schedule(schedule, async () => {
-    logger.info(`executing job ${schedule} at ${new Date()}`);
-    await fn();
-    logger.info('job completed');
-  });
+  cron.schedule(
+    schedule,
+    async () => {
+      logger.info(`executing job ${schedule} at ${new Date()}`);
+      await fn();
+      logger.info('job completed');
+    },
+    { timezone: 'Europe/Vilnius' },
+  );
 };
